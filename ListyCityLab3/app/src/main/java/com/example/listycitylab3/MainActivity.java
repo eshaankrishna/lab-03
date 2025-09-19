a44e0ee149081735e9e38f44cb6583ed19445bef
eshaankrishna <eshaankr@ualberta.ca>
2025-09-19 16:58:57 +0300

    Commited Project to git

---
+++ ListyCityLab3/app/src/main/java/com/example/listycitylab3/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.listycitylab3;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AddCityFragment.AddCityDialogListener {
+
+    private ArrayList<City> dataList;
+    private CityArrayAdapter cityAdapter;
+
+    @Override
+    public void addCity(City city) {
+        cityAdapter.add(city);
+        cityAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void editCity(City originalCity, City editedCity) {
+        int position = dataList.indexOf(originalCity);
+        if (position != -1) {
+            originalCity.setName(editedCity.getName());
+            originalCity.setProvince(editedCity.getProvince());
+            cityAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] cities = { "Edmonton", "Vancouver", "Toronto" };
+        String[] provinces = { "AB", "BC", "ON" };
+
+        dataList = new ArrayList<>();
+        for (int i = 0; i < cities.length; i++) {
+            dataList.add(new City(cities[i], provinces[i]));
+        }
+
+        ListView cityList = findViewById(R.id.city_list);
+        cityAdapter = new CityArrayAdapter(this, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        // Add click listener for editing cities
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            City selectedCity = dataList.get(position);
+            AddCityFragment editFragment = AddCityFragment.newInstance(selectedCity);
+            editFragment.show(getSupportFragmentManager(), "Edit City");
+        });
+
+        FloatingActionButton fab = findViewById(R.id.button_add_city);
+        fab.setOnClickListener(v -> new AddCityFragment().show(getSupportFragmentManager(), "Add City"));
+    }
+}
+
